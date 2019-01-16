5b9c467d047975faf55cd612b1254e9b66e9f63c
15095868104 <591856607@qq.com>
2019-01-16 17:08:43 +0800

    first

---
+++ src/components/Home.vue
@@ -0,0 +1,34 @@
+<style scoped>
+  .carous{
+    width: 600px;
+    height: 400px;
+    margin: 0 auto;
+    background-color: #eca;
+  }
+</style>
+<template>
+  <Carousel class='carous' autoplay v-model="value2" :autoplay-speed="3000" loop>
+    <CarouselItem>
+      <div class="demo-carousel">1</div>
+    </CarouselItem>
+    <CarouselItem>
+      <div class="demo-carousel">2</div>
+    </CarouselItem>
+    <CarouselItem>
+      <div class="demo-carousel">3</div>
+    </CarouselItem>
+    <CarouselItem>
+      <div class="demo-carousel">4</div>
+    </CarouselItem>
+  </Carousel>
+</template>
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      value2: 0
+    }
+  }
+}
+</script>
